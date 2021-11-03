d8faa5cd3d88b4d5c0a4386c82930aaf516dda04
herman <herman@pop-os.localdomain>
2021-11-03 23:23:26 +0100

    Made navbar in javascript (Not done)

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+(function(){
+const navLinks = [
+    {href: 'index.html', text: '<img id="logo" src="images/logo.gif" alt=""> Webtek FC'},
+    {href: 'about.html', text: 'Om oss'},
+    {href: 'membership.html', text: 'Medlemsskap'},
+    {href: 'news.html', text: 'Nyheter'},
+    {href: 'calendar.html', text: 'Kalender'},
+    {href: 'results.html', text: 'Resultater'},
+];
+let nav = document.createElement("nav"),
+navA;
+
+for (var i = 0; i < navLinks.length; i++) {
+    navA = document.createElement("a");
+    navA.href = navLinks[i].href;
+    navA.innerHTML = navLinks[i].text;
+    nav.appendChild(navA)
+}
+
+window.onload = function () {
+    document.body.appendChild(nav);
+}
+}());
