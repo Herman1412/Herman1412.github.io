fe62ecafdd60646a6a8c13b86877ee9da0fa8a2f
herman <herman@pop-os.localdomain>
2021-11-05 23:33:46 +0100

    Added nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,22 @@
-(function(){
 const navLinks = [
     {href: 'index.html', text: '<img id="logo" src="images/logo.gif" alt=""> Webtek FC'},
     {href: 'about.html', text: 'Om oss'},
-    {href: 'membership.html', text: 'Medlemsskap'},
+    {href: 'membership.html', text: 'Bli medlem'},
     {href: 'news.html', text: 'Nyheter'},
     {href: 'calendar.html', text: 'Kalender'},
     {href: 'results.html', text: 'Resultater'},
 ];
+
 let nav = document.createElement("nav"),
 navA;
 
-for (var i = 0; i < navLinks.length; i++) {
+for (let i = 0; i < navLinks.length; i++) {
     navA = document.createElement("a");
     navA.href = navLinks[i].href;
     navA.innerHTML = navLinks[i].text;
     nav.appendChild(navA)
 }
 
-window.onload = function () {
+window.onload = function (){
     document.body.appendChild(nav);
 }
-}());
