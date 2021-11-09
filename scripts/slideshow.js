72aa601803128d007c1719c3976f80b17ff94db1
Herman Saetre <hermansaetre1412@gmail.com>
2021-11-09 16:58:37 +0100

    Made index page

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,20 +1,26 @@
-var i=0
-var image = []
-var time =3000
+let i = 0
+let image = []
+let time =6000
 
-image[1] = "images/slides1.png";
-image[2] = "images/slides2.png";
-image[3] = "images/slides3.png";
-image[4] = "images/slides4.png";
-image[5] = "images/slides5.png";
-image[6] = "images/slides6.png";
+image[0] = "images/slides1.png";
+image[1] = "images/slides2.png";
+image[2] = "images/slides3.png";
+image[3] = "images/slides4.png";
+image[4] = "images/slides5.png";
+image[5] = "images/slides6.png";
+
+let slidesForShow = document.getElementById('slidesForShow');
+slidesForShow.src = image[i];
 
 function changeImg() {
-    document.slide.src = image[i];
-if (i < image.length - 1)
-    i++;
-    else {
-        i=0;
+    if (i < 5){
+        i += 1;
+        slidesForShow.src = image[i];
     }
-settimeout ("changeImg()",time);
+    else{
+        i = 0;
+        slidesForShow.src = image[i];
+    }
+    setTimeout(changeImg, time);
 }
+changeImg()
