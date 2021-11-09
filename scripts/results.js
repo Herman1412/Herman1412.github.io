9a4fafc07074e80d56520fb30b5b0f02d7bff583
Herman Saetre <hermansaetre1412@gmail.com>
2021-11-09 02:01:20 +0100

    Made result page in js

--- scripts/results.js
+++ scripts/results.js
@@ -1,12 +1,59 @@
-//    --Rulette--
+//    --Results--
 
 let results = [
     {
         date: '19.06.2021',
-        ime: '14:20',
+        time: '14:20',
         res: '3-1',
         team1: 'Webtek-FC',
         team2: 'IT-GK FC',
         sted: 'Lerkendal Stadion',
     },
-]
+    {
+        date: '25.05.2021',
+        time: '12:20',
+        res: '4-2',
+        team1: 'Webtek-FC',
+        team2: 'Brazil FC',
+        sted: 'Lerkendal Stadion',
+    },
+    {
+        date: '20.05.2021',
+        time: '18:30',
+        res: '1-1',
+        team1: 'Viking',
+        team2: 'Webtek-FC',
+        sted: 'Viking Stadion',
+    },
+    {
+        date: '04.05.2021',
+        time: '20:00',
+        res: '3-5',
+        team1: 'Webtek-FC',
+        team2: 'NTNUI FC',
+        sted: 'Lerkendal Stadion',
+    },
+    {
+        date: '19.04.2021',
+        time: '14:20',
+        res: '3-1',
+        team1: 'Brazil FC',
+        team2: 'Webtek-FC',
+        sted: 'Lerkendal Stadion',
+    },
+];
+
+let container = document.createElement('div');
+container.setAttribute('class', 'container');
+
+for (let i = 0; i < results.length; i++){
+    div = document.createElement('div');
+    div.setAttribute('class', 'result');
+    for (let key in results[i]){
+        h4 = document.createElement('h4');
+        h4.innerHTML = results[i][key]
+        div.appendChild(h4)
+    }
+    container.appendChild(div);
+}
+document.body.appendChild(container);
